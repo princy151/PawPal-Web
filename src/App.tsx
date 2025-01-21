045d01fc9901db66df15f23347ecefbd793ed50e
princy151 <agrawalprincy151@gmail.com>
2025-01-21 23:13:59 +0545

    Simple Home Page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// import HomePage from './HomePage';
-// import LoginPetSitter from './Login';
+import HomePage from './home.tsx';
+// import LoginPetSitter from './login.tsx';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPetSitter />} /> */}
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/login" element={<LoginPetSitter />} /> */}
       </Routes>
     </Router>
   );
